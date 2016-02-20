a69f9138c794582cb0baa996f01fe274020242e7
Robert.Finan <Robert.Finan@surface01.anecon.com>
2016-02-20 19:37:49 +0100

    Got the pointed pair and hidden single working

--- public/js/algorithm.js
+++ public/js/algorithm.js
@@ -9,5 +9,20 @@ var isPointingPair = function(pair, cells) {
 };
 
 var hasHiddenSingle = function(cell, cells) {
+   
+   var numberOfCandidates = cell.candidates.length;
+   
+    for (var i = 0; i < numberOfCandidates; i++){
+      var unique = true;
+      for (var j = 0; j < cells.length; j++) {
+          if (cells[j].candidates.search(cell.candidates[i]) !== -1) {
+              unique = false;
+          };
+      };
+      if (unique) {
+          return cell.candidates[i];
+      }
+    };
     
+    return "0";
 };
